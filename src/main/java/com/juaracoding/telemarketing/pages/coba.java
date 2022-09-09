fb32047f28dce7f10df43a2b01761de104842a52
desyjihan <desyjihan7@gmail.com>
2022-09-09 23:01:20 +0700

    coba

---
+++ src/main/java/com/juaracoding/telemarketing/pages/coba.java
@@ -0,0 +1,10 @@
+package com.juaracoding.telemarketing.pages;
+
+public class coba {
+
+	public static void main(String[] args) {
+
+		System.out.println("Main App");
+		System.out.println("Update Main App");
+	}
+}
